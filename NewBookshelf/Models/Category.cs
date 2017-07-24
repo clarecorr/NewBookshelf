d1e3fee2d213567e73a60fec7db849bb6e8281c7
clarecorr <clare.e.corrigan@gmail.com>
2017-07-24 15:00:50 -0400

    added controllers and views

--- NewBookshelf/Models/Category.cs
+++ NewBookshelf/Models/Category.cs
@@ -11,6 +11,8 @@ namespace NewBookshelf.Models
     {
         [Key]
         public int ID { get; set; }
+
+        [Display(Name = "Category")]
         public string Name { get; set; }
         public virtual ICollection<Book> Books { get; set; } //ICollection also a navigation property
     }
