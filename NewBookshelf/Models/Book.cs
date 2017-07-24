5874d86d9b9c5e3f1855871611765ceb95c361ac
clarecorr <clare.e.corrigan@gmail.com>
2017-07-24 14:17:29 -0400

    added book and category models

---
+++ NewBookshelf/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewBookshelf.Models
+{
+    public class Book
+    {
+        [Key] //primary key
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+        //navigation property (line above)
+      
+    }
+}
---
+++ NewBookshelf/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NewBookshelf.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; } //ICollection also a navigation property
+    }
+}
